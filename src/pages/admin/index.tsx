8475d7c4aba146d2b5c9604797760c29cdb626fe
KEHhyeon <kth050213@naver.com>
2022-11-01 01:36:36 +0900

    add:page구조

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,13 @@
+import * as S from "./style";
+
+export default function Admin() {
+  return (
+    <S.Contain1>
+      <></>
+      <S.Contain2>
+        <></>
+        <S.Contain3></S.Contain3>
+      </S.Contain2>
+    </S.Contain1>
+  );
+}
---
+++ src/pages/admin/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+export const Contain1 = styled.div`
+  display: flex;
+`;
+export const Contain2 = styled.div`
+  display: flex;
+`;
+export const Contain3 = styled.div`
+  display: flex;
+`;
