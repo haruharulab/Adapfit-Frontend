1706e315c11b52f87962bd0e263524772d4e5c85
KEHhyeon <kth050213@naver.com>
2022-11-22 15:31:32 +0900

    add:footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Plan } from "./pages/planlist";
 import { Admin } from "./pages/admin";
 import { Home } from "./pages/home";
 import { Header } from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="*" element={<Home />} />
-        <Route path="/plan" element={<Plan />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="/plan" element={<Plan />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
