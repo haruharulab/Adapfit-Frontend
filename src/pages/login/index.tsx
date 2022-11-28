d4f8dab65e36d809eef53ed98cca3ac79ef69c4c
KEHhyeon <kth050213@naver.com>
2022-11-29 08:43:31 +0900

    add:login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,52 @@
+import { useState } from "react";
+import { LOGIN_URL } from "../../constant/url";
 import * as S from "./style";
 export default function Login() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const submit = ({ id, password }: { id: string; password: string }) => {
+    let data = {
+      id: id,
+      password: password,
+    };
+
+    let config = {
+      method: "post",
+      url: LOGIN_URL,
+      headers: {},
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(response.data.accessToken.value);
+        setCookie("accessToken", response.data.accessToken.value);
+        setCookie("refreshToken", response.data.refreshToken.value);
+        localStorage.setItem("accessToken", response.data.accessToken.value);
+        localStorage.setItem("refreshToken", response.data.refreshToken.value);
+        setIsLogin(true);
+        router.push("/");
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log(data);
+        alert(error.response.data.message);
+      });
+  };
   return (
     <S.Contain>
       <S.LoginWrapper>
         <span>로그인</span>
-        <S.Input placeholder="아이디를 입력해주세요"></S.Input>
-        <S.Input placeholder="비밀번호를 입력해주세요"></S.Input>
+        <S.Input
+          placeholder="아이디를 입력해주세요"
+          onChange={(e) => setId(e.target.value)}
+        ></S.Input>
+        <S.Input
+          placeholder="비밀번호를 입력해주세요"
+          onChange={(e) => setPassword(e.target.value)}
+        ></S.Input>
         <S.NoneId>계정이 없으신가요?</S.NoneId>
-        <S.Btn_login>로그인</S.Btn_login>
+        <S.Btn_login onClick={() => {}}>로그인</S.Btn_login>
       </S.LoginWrapper>
     </S.Contain>
   );
