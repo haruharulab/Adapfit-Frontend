c6761f383baff92fa8cc6a4a7d43277dedd9edc3
KEHhyeon <kth050213@naver.com>
2022-12-03 23:06:12 +0900

    fix::gap

--- src/pages/planlist/style.ts
+++ src/pages/planlist/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 export const Contain = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 30px;
+  gap: 29px;
   margin: 80px 0px;
 `;
 export const Category = styled.div`
