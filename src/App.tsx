07e5b85f0b17164f2b33ab50970504d6fae43cb4
leehj050211 <leehj050211@gmail.com>
2022-12-05 14:08:48 +0900

    ADD: 채널톡 서비스

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,27 @@ import Footer from "./components/footer";
 import Notice from "./pages/notice";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
+import channelService from "./components/channel/channelService";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="*" element={<Home />} />
-          <Route path="/plan" element={<Plan />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/notice" element={<Notice />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
-  );
+    channelService.boot({
+        "pluginKey": process.env.REACT_APP_CHANNEL_SERVICE_PLUGIN_KEY
+    });
+
+    return (
+        <BrowserRouter>
+            <Header />
+                <Routes>
+                    <Route path="*" element={<Home />} />
+                    <Route path="/plan" element={<Plan />} />
+                    <Route path="/admin" element={<Admin />} />
+                    <Route path="/notice" element={<Notice />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
