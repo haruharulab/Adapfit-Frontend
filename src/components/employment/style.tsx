214a141635dcd5cfbf5e2ba2cdafec6eb1a3214e
KEHhyeon <kth050213@naver.com>
2022-12-12 20:58:37 +0900

    add:채용목록페이지

--- src/components/employment/style.tsx
+++ src/components/employment/style.tsx
@@ -2,5 +2,20 @@ import styled from "styled-components";
 
 export const Contain = styled.div`
   width:100%;
-  height: ;
+  background-color: white;
+  display: flex;
+  justify-content: space-around;
+  height: 80px;
+  align-items: center;
+  span{
+    width: 15%;
+    text-align: center;
+    line-height: 40px ;
+  }
+  border-radius: 10px;
+  margin-bottom: 20px;
+`
+export const Name = styled.div`
+  margin-left: 10px;
+  width: 45%;
 `
