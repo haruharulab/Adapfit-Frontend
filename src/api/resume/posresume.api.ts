eee9377837b7a3c4f1c15a8ae103a82d6d48df02
KEHhyeon <kth050213@naver.com>
2022-12-16 12:34:48 +0900

    add:post요청

--- src/api/resume/posresume.api.ts
+++ src/api/resume/posresume.api.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 import { customAxios } from "../../lib/axios/customaxios";
 
 export default function posResume(formData: any) {
-    customAxios.post('resume', formData, {headers: {
-      'Content-Type': 'multipart/form-data'
-    }})
-        .then(()=>alert("지원 완료!!"))
-        .catch((e)=>{alert(e)})
+  customAxios
+    .post("resume", formData)
+    .then(() => alert("지원 완료!!"))
+    .catch((e) => {
+      alert(e);
+    });
 }
