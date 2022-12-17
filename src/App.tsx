c0fbcb042caae053969928dc955c923e136cb03e
KEHhyeon <kth050213@naver.com>
2022-12-17 22:08:07 +0900

    a

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import { PlanHome } from "./pages/plan/index";
+import { Admin } from "./pages/admin";
+import { Home } from "./pages/home";
+import { Header } from "./components/header";
+import Footer from "./components/footer";
+import Notice from "./pages/notice";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Employment from "./pages/employment";
+import channelService from "./components/channel/channelService";
+import PlanDetail from "./pages/plan/detail/detail";
+import AdminPageRoute from "./pages/admin/route";
+
+function App() {
+  channelService.boot({
+      "pluginKey": process.env.REACT_APP_CHANNEL_SERVICE_PLUGIN_KEY
+  });
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="/plan" element={<PlanHome />} />
+          <Route path="/plan/:id" element={<PlanDetail />} />
+          <Route path="/admin/*" element={<AdminPageRoute />} />
+          <Route path="/employment" element={<Employment />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+export default App;
