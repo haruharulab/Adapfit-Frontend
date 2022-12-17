8dc841d702039ff6fc7cbc627515884472673c47
KEHhyeon <kth050213@naver.com>
2022-12-17 20:51:47 +0900

    add:채용공고 검색기능

--- src/pages/employment/index.tsx
+++ src/pages/employment/index.tsx
@@ -1,20 +1,71 @@
-import * as S from './style'
-import {Item} from "../../components/employment";
+import * as S from "./style";
+import { Item } from "../../components/employment";
+import { useState } from "react";
+import getrecuritmentApi from "../../api/recruitment/getrecuritment.api";
 
 export default function Employment() {
-    return (
-        <S.Contain>
-            <h2>지금 채용 중인 포지션이에요!</h2>
-            <S.Header>
-                <S.Search placeholder={"원하는 포지션을 검색해주세요"}></S.Search>
-                <span>채용 직군</span>
-                <span>경력</span>
-                <span>근무지역</span>
-            </S.Header>
-            <Item title={"피트니스센터메니저"} jobGroup={"매니저"} career={"1"} employmentArea={"부산"}/>
-            <Item title={"피트니스센터메니저"} jobGroup={"매니저"} career={"1"} employmentArea={"부산"}/>
-            <Item title={"피트니스센터메니저"} jobGroup={"매니저"} career={"1"} employmentArea={"부산"}/>
-
-        </S.Contain>
-    )
+  const [jobGroup, setJobGroup] = useState("");
+  const [career, setCareer] = useState("");
+  const [employmentPattern, setEmploymentPattern] = useState("");
+  const [data, setData] = useState<any>([]);
+  const getData = async () => {
+    if (jobGroup == "" || career == "" || employmentPattern == "") {
+      alert("전부 선택해주세요!!");
+      return false;
+    }
+    const req = {
+      jobGroup: jobGroup,
+      career: career,
+      employmentPattern: employmentPattern,
+    };
+    const temp = await getrecuritmentApi.getRecuritment(req);
+    setData(temp);
+  };
+  return (
+    <S.Contain>
+      <h2>지금 채용 중인 포지션이에요!</h2>
+      <S.Header>
+        <S.Search>
+          <select onChange={(e) => setJobGroup(e.target.value)}>
+            <option value={""}>채용직군</option>
+            <option value={"1"}>1</option>
+            <option value={"2"}>2</option>
+            <option value={"3"}>3</option>
+          </select>
+          <select onChange={(e) => setCareer(e.target.value)}>
+            <option value={""}>채용직군</option>
+            <option value={"1"}>1</option>
+            <option value={"2"}>2</option>
+            <option value={"3"}>3</option>
+          </select>
+          <select onChange={(e) => setEmploymentPattern(e.target.value)}>
+            <option value={""}>채용직군</option>
+            <option value={"1"}>1</option>
+            <option value={"2"}>2</option>
+            <option value={"3"}>3</option>
+          </select>
+          <button onClick={() => getData()}>검색</button>
+        </S.Search>
+        <span>채용 직군</span>
+        <span>경력</span>
+        <span>근무지역</span>
+      </S.Header>
+      {data.map((i: any) => {
+        return (
+          <Item
+            title={i.title}
+            jobGroup={i.jobGroup}
+            carre={i.career}
+            employmentArea={i.employmentArea}
+          />
+        );
+      })}
+      <Item
+        title={"피트니스센터메니저"}
+        jobGroup={"매니저"}
+        career={"1"}
+        employmentArea={"부산"}
+      />
+    </S.Contain>
+  );
 }
--- src/pages/employment/style.tsx
+++ src/pages/employment/style.tsx
@@ -6,28 +6,34 @@ export const Header = styled.div`
   margin-bottom: 20px;
   display: flex;
   justify-content: space-between;
-  span{
+  span {
     background-color: #f2f2f2;
     width: 15%;
     text-align: center;
-    line-height: 40px ;
+    line-height: 40px;
   }
   * {
     border-radius: 10px;
   }
-`
+`;
 
-export const Search = styled.textarea`
-  resize: none;
+export const Search = styled.div`
   width: 45%;
   height: 40px;
-  line-height: 40px;
-  padding-left: 10px;
   background-color: white;
   border: 0px;
-`
+  select {
+    font-size: 1.2rem;
+    width: 25%;
+    height: 100%;
+  }
+  button {
+    width: 25%;
+    height: 100%;
+  }
+`;
 export const Contain = styled.div`
   margin: auto;
   margin-top: 10px;
   width: 90%;
-`
+`;
