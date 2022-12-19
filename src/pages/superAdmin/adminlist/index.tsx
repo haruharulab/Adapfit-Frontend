678a378e0f31b8cf3bfb28ba7e6002d70f4cf263
KEHhyeon <kth050213@naver.com>
2022-12-20 08:55:28 +0900

    add::superadmin/adminlist page

--- src/pages/superAdmin/adminlist/index.tsx
+++ src/pages/superAdmin/adminlist/index.tsx
@@ -16,7 +16,7 @@ export default function AdminList() {
     <S.Contain>
       <h2>지금 채용 중인 포지션이에요!</h2>
       <S.Header>
-        <S.Search>&nbsp;&nbsp;&nbsp;&nbsp;관리자검색</S.Search>
+        <S.Search>&nbsp;&nbsp;&nbsp;&nbsp; 관리자검색</S.Search>
         <span>이메일</span>
         <span>소속</span>
         <span>연락처</span>
