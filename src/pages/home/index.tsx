763067b147eb9c7fb0284192b81342fc7c0d57d3
yunaeun <naeun2964@naver.com>
2022-12-19 20:15:00 +0900

    style :: 메인 배너 화살표, 채용 페이지 스타일 변경

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import * as S from "./style";
-import {Swiper, SwiperSlide} from "swiper/react";
-import SwiperCore, {Navigation, Pagination, Autoplay} from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -11,67 +11,70 @@ import { getBannerList } from "../../apis/banner.api";
 import { getPlanList } from "../../apis/plan.api";
 import { Plan } from "../../types/plan.type";
 import PlanList from "../../components/plan/planList";
+
 SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 export const Home = () => {
-    const [bannerList, setBannerList] = useState<Banner[]>([]);
-    const [planList, setPlanList] = useState<Plan[]>([]);
-    
-    useEffect(() => {
-        (async () => setBannerList(await getBannerList()))();
-        (async () => setPlanList(await getPlanList()))();
-    }, []);
+  const [bannerList, setBannerList] = useState<Banner[]>([]);
+  const [planList, setPlanList] = useState<Plan[]>([]);
 
-    return (
-        <S.HomeWrap>
-            <S.Banner>
-                <Swiper
-                    className="banner"
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    navigation
-                    pagination={{clickable: true}}
-                    autoplay={{delay: 3000}}
-                >
-                    {bannerList.map(banner => (
-                        <SwiperSlide>
-                            <S.BannerImgWrap>
-                                <S.BannerImg src={banner.fileUrl} className="slider"/>
-                                <S.BannerBackgroundImg src={banner.fileUrl}/>
-                            </S.BannerImgWrap>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </S.Banner>
-            <S.IntroduceWrap>
-                <h3>
-                    특별한 당신을 위한
-                    <br />
-                    특별한 운동,
-                </h3>
-                <S.Adapfit>
-                    어댑핏!
-                </S.Adapfit>
-                <img src='/image/adapfit_introduce.png' alt="" />
-            </S.IntroduceWrap>
-            <S.PlanGallery>
-                <PlanList planList={planList} />
-            </S.PlanGallery>
-            <S.PlanDetailLink to='/plan'>플랜 더보기</S.PlanDetailLink>
-            <S.DownContain>
-                <S.DownloadLink target='_blank' href="https://apps.apple.com/us/app/%EC%96%B4%EB%8C%91%ED%95%8F/id1618654622?l=ko">
-                    <span>
-                        IOS 다운로드
-                    </span>
-                    <img src='/image/ios.png'/>
-                </S.DownloadLink>
-                <S.DownloadLink target='_blank' href="https://play.google.com/store/apps/details?id=kr.adapfit.app">
-                    <span>
-                        안드로이드 다운로드
-                    </span>
-                    <img src='/image/android.png'/>
-                </S.DownloadLink>
-            </S.DownContain>
-        </S.HomeWrap>
-    );
+  useEffect(() => {
+    (async () => setBannerList(await getBannerList()))();
+    (async () => setPlanList(await getPlanList()))();
+  }, []);
+
+  return (
+    <S.HomeWrap>
+      <S.Banner>
+        <S.LeftArrow className="prev" size="40" />
+        <S.RightArrow className="next" size="40" />
+        <Swiper
+          className="banner"
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000 }}
+        >
+          {bannerList.map((banner) => (
+            <SwiperSlide>
+              <S.BannerImgWrap>
+                <S.BannerImg src={banner.fileUrl} className="slider" />
+                <S.BannerBackgroundImg src={banner.fileUrl} />
+              </S.BannerImgWrap>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </S.Banner>
+      <S.IntroduceWrap>
+        <h3>
+          특별한 당신을 위한
+          <br />
+          특별한 운동,
+        </h3>
+        <S.Adapfit>어댑핏!</S.Adapfit>
+        <img src="/image/adapfit_introduce.png" alt="" />
+      </S.IntroduceWrap>
+      <S.PlanGallery>
+        <PlanList planList={planList} />
+      </S.PlanGallery>
+      <S.PlanDetailLink to="/plan">플랜 더보기</S.PlanDetailLink>
+      <S.DownContain>
+        <S.DownloadLink
+          target="_blank"
+          href="https://apps.apple.com/us/app/%EC%96%B4%EB%8C%91%ED%95%8F/id1618654622?l=ko"
+        >
+          <span>IOS 다운로드</span>
+          <img src="/image/ios.png" />
+        </S.DownloadLink>
+        <S.DownloadLink
+          target="_blank"
+          href="https://play.google.com/store/apps/details?id=kr.adapfit.app"
+        >
+          <span>안드로이드 다운로드</span>
+          <img src="/image/android.png" />
+        </S.DownloadLink>
+      </S.DownContain>
+    </S.HomeWrap>
+  );
 };
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -1,3 +1,7 @@
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Container } from "../../components/common/container/style";
@@ -19,6 +23,28 @@ export const BannerImgWrap = styled.div`
   position: relative;
   overflow: hidden;
   user-select: none;
+`;
+
+const Arrow = styled(IoIosArrowDropleftCircle)`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  color: rgba(255, 255, 255, 0.6);
+`;
+
+const RArrow = styled(IoIosArrowDroprightCircle)`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  color: rgba(255, 255, 255, 0.6);
+`;
+
+export const LeftArrow = styled(Arrow)`
+  left: 50px;
+`;
+
+export const RightArrow = styled(RArrow)`
+  right: 50px;
 `;
 
 export const BannerImg = styled.img`
@@ -45,6 +71,16 @@ export const Banner = styled.div`
   @media screen and (max-width: 960px) {
     height: 30vh;
   }
+  .swiper-button-next {
+    &::after {
+      display: none;
+    }
+  }
+  .swiper-button-prev {
+    &::after {
+      display: none;
+    }
+  }
 `;
 
 export const IntroduceWrap = styled.div`
@@ -59,11 +95,11 @@ export const IntroduceWrap = styled.div`
     margin-top: 100px;
     width: 100%;
   }
-  `;
+`;
 
 export const Adapfit = styled.h3`
   color: #ff5a28;
-  `;
+`;
 
 export const PlanGallery = styled.div`
   margin: 150px 0 30px;
@@ -90,9 +126,9 @@ export const DownContain = styled.div`
   gap: 50px;
   text-align: center;
   @media screen and (max-width: 960px) {
-      flex-direction: column;
+    flex-direction: column;
   }
-`
+`;
 export const DownloadLink = styled.a`
   display: inline-flex;
   justify-content: space-between;
@@ -110,8 +146,8 @@ export const DownloadLink = styled.a`
     width: 70px;
     height: auto;
   }
-  transition: background-color .25s;
-  background-color: #FFF;
+  transition: background-color 0.25s;
+  background-color: #fff;
   cursor: pointer;
   &:hover {
     background-color: #00000022;
@@ -119,13 +155,13 @@ export const DownloadLink = styled.a`
   @media screen and (max-width: 450px) {
     width: 250px;
     span {
-        font-size: 20px;
+      font-size: 20px;
     }
     img {
       width: 60px;
     }
   }
-`
+`;
 
 export const Counselor = styled.div`
   position: fixed;
@@ -135,4 +171,4 @@ export const Counselor = styled.div`
   .counselor {
     size: 24;
   }
-`
+`;
