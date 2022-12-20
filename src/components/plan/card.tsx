ace9a322c3718dd76b9773d922f3ee8d016e17bc
KEHhyeon <kth050213@naver.com>
2022-12-20 12:03:26 +0900

    fix::메인 화면에서 plan상세 페이지로 안가지는 버그

--- src/components/plan/card.tsx
+++ src/components/plan/card.tsx
@@ -7,7 +7,7 @@ interface PlanCardProps {
 
 export default function PlanCard({ plan }: PlanCardProps) {
   return (
-    <S.CardContain to={`${plan.planId}`}>
+    <S.CardContain to={`/plan/${plan.planId}`}>
       <S.Img img={plan.thumbnail} />
       <S.Content>{plan.title}</S.Content>
     </S.CardContain>
